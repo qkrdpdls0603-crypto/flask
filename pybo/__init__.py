b4489ac36dad125312eb16afbb5e48b298245e69
qkrdpdls0603-crypto <qkrdpdls0603@gmail.com>
2026-03-31 13:14:11 +0900

    260331

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():  # 애플리케이션 팩토리
     app = Flask(__name__)
+    app.config.from_object('config')
+
+    #ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
 # 블루프린트 등록
     from .views import main_views
