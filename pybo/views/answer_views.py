67e3621f430a84d2af207f303a0999a9d1dd5f1d
qkrdpdls0603-crypto <qkrdpdls0603@gmail.com>
2026-04-03 17:53:13 +0900

    260403

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,18 +1,27 @@
 from datetime import datetime
 
-from flask import Blueprint, redirect, url_for, request
+from flask import Blueprint, redirect, url_for, request, render_template
 
 from pybo import db
+from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=['POST'])
 def create(question_id):
+    form = AnswerForm(request.form)
+    # 답변 등록할 질문을 DB 조회
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
-    answer = Answer(question=question, content=content, create_date=datetime.now())
-    db.session.add(answer)
-    db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    #사용자가 입력한 form 태그의 'content' 속성의 값을 변수에 넣음
+    if form.validate_on_submit():
+        content = request.form.get('content')
+        #Answer 객체 생성
+        answer = Answer(content=content, create_date=datetime.now())
+        # 해당 질문에 답변 등록
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+
+    return render_template('question/question_detail.html', question=question, form=form)
 
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from datetime import datetime
+from pybo import db
 
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from pybo.forms import QuestionForm
 from pybo.models import Question
 
 bp = Blueprint('question', __name__, url_prefix='/question')
@@ -13,3 +17,13 @@ def _list():
 def detail(question_id):
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question)
+
+@bp.route('/create/', methods=['GET', 'POST'])
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html',form=form)
